7bbcfae71210164ca4605bc04c7d66108dd57680
vsergey3d <vsergey3d@gmail.com>
2016-02-06 10:36:39 +0700

    fix NOEXCEPT macro definition

--- src/draw.h
+++ src/draw.h
@@ -13,12 +13,14 @@
 
 #if defined(__clang__)
 #if __has_feature(cxx_noexcept)
-#define NOEXCEPT noexcept
+#define HAS_NOEXCEPT
 #endif
-#endif
-#if !defined(__clang__) && \
-	defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ * 10 + __GNUC_MINOR__ >= 46 || \
+#elif !defined(__clang__) && \
+    defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ * 10 + __GNUC_MINOR__ >= 46 || \
     defined(_MSC_FULL_VER) && _MSC_FULL_VER >= 190023026
+#define HAS_NOEXCEPT
+#endif
+#ifdef HAS_NOEXCEPT
 #define NOEXCEPT noexcept
 #else
 #define NOEXCEPT
